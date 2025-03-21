683ade844d1e1b175e849c2d8af86595266077ed
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2025-03-21 12:29:36 +0100

    added models and admin for Movie and Director

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie, Director
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "year", "director"]
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "birth_year"]
 
 # Register your models here.
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Director, DirectorAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=300)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True, default="")
+    director = models.ForeignKey('Director', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.title} ({self.year})"
+
+class Director(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_year})"
+
