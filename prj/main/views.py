eed85fe30077340b13910ce97eda8d629b320e2c
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2025-05-02 10:21:54 +0200

    added homepage view and search

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Movie
+
+def get_homepage(request):
+    movies = Movie.objects.all().order_by('title')
+
+    # check if search parameter is in the request
+    if request.GET.get("search"):
+        movies = movies.filter(title__icontains=request.GET.get("search"))
+
+    context = {
+        "svatek": "Libor",
+        # SELECT * from Movies ORDER BY 'title' LIMIT 10;
+        "movies": movies
+    }
+    return render(
+        request, "main/homepage.html", context
+    )
